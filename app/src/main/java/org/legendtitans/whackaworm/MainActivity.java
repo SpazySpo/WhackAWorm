d04330383ea604184922eddbf024501516d5e653
SpazySpoo <lapagliadm@s.dcsdk12.org>
2021-02-17 11:26:50 -0700

    On touch code implemented

--- app/src/main/java/org/legendtitans/whackaworm/MainActivity.java
+++ app/src/main/java/org/legendtitans/whackaworm/MainActivity.java
@@ -10,5 +10,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView imageViewPlay = (ImageView) findViewById(R.id.playImageView);
+        imageViewPlay.setOnTouchListener(new View.OnTouchListener() {
+
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                // TODO Auto-generated method stub
+                Log.i("imageviewandontouchlistener", "playImageView onTouch");
+                return false;
+            }
+        });
     }
 }
